38626672c4861afb9866a1935976ff99bbf9cf91
Soumya-De <soumyadey820@gmail.com>
2024-07-12 01:01:44 +0530

    basic app created

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.currencyconverterapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btn: Button = findViewById(R.id.button)
+        val output: TextView = findViewById(R.id.output)
+        val input: EditText = findViewById(R.id.rupees)
+        val dollar: TextView = findViewById(R.id.DOLLAR)
+        val inr: TextView = findViewById(R.id.INR)
+        val title: TextView = findViewById(R.id.Title)
+        btn.setOnClickListener {
+            val amount = input.text.toString().toDouble()
+            output.text = (amount / 83.5680).toString()
         }
     }
 }
